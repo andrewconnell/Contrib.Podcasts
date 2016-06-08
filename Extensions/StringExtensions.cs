c9135bc6467b73dfa88efd10324980333877d33a
Andrew Connell <me@andrewconnell.com>
2016-06-08 10:34:39 -0400

    fix(enums): change camelcase enums to spaces
    Added extension method that supports adding spaces to strings in camelcase so "ThisText" turns into "This Text". Updated all admin pages that displayed these enums.
    
    Closes #2.

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Contrib.Podcasts.Extensions {
+  public static class StringExtensions {
+    public static string SplitCamelCase (this string str) {
+      return Regex.Replace(
+        Regex.Replace(str,
+                      @"(\P{Ll})(\P{Ll}\p{Ll})",
+                      "$1 $2"),
+        @"(\p{Ll})(\P{Ll})",
+        "$1 $2");
+    }
+  }
+}
